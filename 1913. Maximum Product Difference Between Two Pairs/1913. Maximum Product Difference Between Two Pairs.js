3d40f4da33c69425ac07efc381ac57240b81f056
NeKoPoWerrr <58406997+NeKoPoWerrr@users.noreply.github.com>
2021-06-29 01:31:30 +0800

    Add files via upload

---
+++ 1913. Maximum Product Difference Between Two Pairs/1913. Maximum Product Difference Between Two Pairs.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maxProductDifference = function(nums) {
+    nums.sort( (a,b) => a-b );
+    return nums[nums.length-1]*nums[nums.length-2] - nums[0]*nums[1]
+};
+
+/*
+*/
